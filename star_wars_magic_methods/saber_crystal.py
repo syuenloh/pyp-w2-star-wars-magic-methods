65f05a274ee148feb37b0b1ed58449a7d2361ef2
syuenloh <syuenloh.ds@gmail.com>
2018-10-05 22:44:00 +0800

    complete answers

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -9,87 +9,48 @@ class SaberCrystal:
        return self.color==other.color
    
    def __add__(self,other):
-       if type(other)==tuple:
-           self.red=self.red+other[0]    
-           self.green=self.green+other[1]
-           self.blue=self.blue+other[2]
-           self.color=(self.red,self.green,self.blue)
-           while self.color<=(255,255,255):
-               return self
-           else:
-               self.color=(255,255,255)
-               return self
-       else:
-           self.red=self.red+other.red
-           self.blue=self.blue+other.blue
-           self.green=self.green+other.green
-           self.color=(self.red,self.green,self.blue)
-           while self.color<=(255,255,255):
-               return self
-           else:
-               self.color=(255,255,255)
-               return self
-
-       
-   def __iadd__(self,other):
-       if type(other)==tuple:
-           self.red=self.red+other[0]
-           self.green=self.green+other[1]
-           self.blue=self.blue+other[2]
-           self.color=(self.red,self.green,self.blue)
-           while self.color<=(255,255,255):
-               return self
-           else:
-               self.color=(255,255,255)
-               return self
-
-       else:
-           self.red=self.red+other.red
-           self.blue=self.blue+other.blue
-           self.green=self.green+other.green
-           self.color=(self.red,self.green,self.blue)
-           while self.color<=(255,255,255):
-               return self
-           else:
-               self.color=(255,255,255)
-               return self
+        if type(other)==tuple:
+            color_list=[x+y for x,y in zip(self.color,other)]
+        else:
+            color_list=[x+y for x,y in zip(self.color,other.color)]
+        if max(color_list)>255:
+            self.color = tuple([255 if i > 255 else i for i in color_list])
+        else:
+            self.color=tuple(color_list)
+        return self
    
+   def __iadd__(self,other): 
+        if type(other)==tuple:
+            color_list=[x+y for x,y in zip(self.color,other)]
+        else:
+            color_list=[x+y for x,y in zip(self.color,other.color)]
+        if max(color_list)>255:
+            self.color = tuple([255 if i > 255 else i for i in color_list])
+        else:
+            self.color=tuple(color_list)
+        return self
    def __sub__(self,other):
         if type(other)==tuple:
-            self.red=self.red-other[0]
-            self.green=self.green-other[1]
-            self.blue=self.blue-other[2]    
-            self.color=(self.red,self.green,self.blue)
-            while self.color<=(255,255,255):
-                return self
-            else:
-                self.color=(0,0,0)
-                return self
+            color_list=[x-y for x,y in zip(self.color,other)]
         else:
-            self.red=self.red-other.red
-            self.blue=self.blue-other.blue
-            self.green=self.green-other.green
-            self.color=(self.red,self.green,self.blue)
-            while self.color<=(255,255,255):
-                return self
-            else:
-                self.color=(0,0,0)
-                return self
-   
+            color_list=[x-y for x,y in zip(self.color,other.color)]
+        if min(color_list)<0:
+            self.color = tuple([0 if i < 0 else i for i in color_list])
+        else:
+            self.color=tuple(color_list)
+        return self
+                
    def __isub__(self,other):
-       if type(other)==tuple:
-           self.red=self.red-other[0]
-           self.green=self.green-other[1]
-           self.blue=self.blue-other[2]
-           self.color=(self.red,self.green,self.blue)
-           return self
-       else:
-           self.red=self.red-other.red
-           self.blue=self.blue-other.blue
-           self.green=self.green-other.green
-           self.color=(self.red,self.green,self.blue)
-           return self
-       
+        if type(other)==tuple:
+            color_list=[x-y for x,y in zip(self.color,other)]
+        else:
+            color_list=[x-y for x,y in zip(self.color,other.color)]
+        if min(color_list)<0:
+            self.color = tuple([0 if i < 0 else i for i in color_list])
+        else:
+            self.color=tuple(color_list)
+        return self    
+   
    def __contains__(self,other):
         if type(other)==tuple:
             return any(x == y for x, y in zip(self.color, other) if x!=0)
